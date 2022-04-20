daa95f2920638b53e193546b2e37dd2ac216a14d
sav7972 <sav7972@rit.edu>
2022-04-20 18:10:23 -0400

    Motin Detection File orangized

---
+++ ballAlogrithm2.py
@@ -0,0 +1,44 @@
+'''
+ Author: Alejandro(Steven)
+ Date: April.20-2022
+ File Name: ballAlgorithm2.py
+ File Description: 
+    Using motion detection an image with only the objects that moved will be show in the new image.
+    With the use of findcontours the center of the ball will be used to ge the ball location in the image
+'''
+
+import cv2
+
+# cap = cv2.VideoCapture(0)
+path = 'videos/bounces/orangeBallBouncePingPongTable1.mov'
+path1 = 'videos/bounds/orangeBallMiddleBound2.mov'
+path2 = 'videos/bounds/orangeBallRightBound2.mov'
+cap = cv2.VideoCapture(path)
+
+# _, firstFrame = cap.read()
+# firstFrameGray = cv2.cvtColor(firstFrame,cv2.COLOR_BGR2GRAY)
+# firstFrameGray = cv2.GaussianBlur(firstFrameGray,(5,5),0)
+
+substractor = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    success, video = cap.read()
+    
+    videoGray = cv2.cvtColor(video,cv2.COLOR_BGR2GRAY)
+    
+    imgMotionDetection = substractor.apply(videoGray)
+    
+    # _, mask = cv2.threshold(videoGray,100,255,cv2.THRESH_BINARY)
+    # mask =  cv2.GaussianBlur(mask,(5,5),0)
+    # mask = cv2.erode(mask, None, iterations=2)
+    # mask = cv2.dilate(mask, None, iterations=2)
+    # ImgMotionDetectionManual = cv2.absdiff(firstFrameGray,imgGray)
+
+    videoGray = cv2.resize(videoGray, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
+    cv2.imshow("Video Gray",videoGray)
+    
+    imgMotionDetection = cv2.resize(imgMotionDetection, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
+    cv2.imshow("Motion Detection", imgMotionDetection)
+
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        break
--- motionDetection.py
+++ motionDetection.py
@@ -18,26 +18,19 @@ while True:
     
     videoGray = cv2.cvtColor(video,cv2.COLOR_BGR2GRAY)
     
-    vidMotionDetection = substractor.apply(videoGray)
+    imgMotionDetection = substractor.apply(videoGray)
     
-    _, thresholdMask = cv2.threshold(videoGray,100,255,cv2.THRESH_BINARY)
-    # imgDiff = cv2.absdiff(firstFrameGray,imgGray)
-    
-    # imgDiff =  cv2.GaussianBlur(imgDiff,(5,5),0)
-    
-    # _, imgDiff = cv2.threshold(imgDiff, 25,255, cv2.THRESH_BINARY)
+    # _, mask = cv2.threshold(videoGray,100,255,cv2.THRESH_BINARY)
+    # mask =  cv2.GaussianBlur(mask,(5,5),0)
+    # mask = cv2.erode(mask, None, iterations=2)
+    # mask = cv2.dilate(mask, None, iterations=2)
+    # ImgMotionDetectionManual = cv2.absdiff(firstFrameGray,imgGray)
 
-    videoGray = cv2.resize(videoGray, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
-    vidMotionDetection = cv2.resize(vidMotionDetection, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
-
-    video = cv2.resize(video, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
-    cv2.imshow("Video",video)
     videoGray = cv2.resize(videoGray, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
     cv2.imshow("Video Gray",videoGray)
-    thresholdMask = cv2.resize(thresholdMask, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
-    cv2.imshow("Threshhold Mask",thresholdMask)
-    cv2.imshow("Motion Detection", vidMotionDetection)
-    # cv2.imshow("Video Diff",imgDiff)
+    
+    imgMotionDetection = cv2.resize(imgMotionDetection, (0,0), None, 0.25,0.25) # Resized the img to fourth its size
+    cv2.imshow("Motion Detection", imgMotionDetection)
 
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
