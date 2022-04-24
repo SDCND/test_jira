b292cb85730b864c80e7872551c8a93b48518fc0
sav7972 <sav7972@rit.edu>
2022-04-23 20:02:17 -0400

    Deleted Few files

--- TestFile/labs/homework1.py
+++
@@ -1,8 +0,0 @@
-# Steven Varada Salazar
-# ESD2 - Homework 1
-# Jan. 16 - 2022
-
-num1 = input("Enter the first number:")
-num2 = input("Enter the second number:")
-result = float(num1) + float(num2)
-print(result)
